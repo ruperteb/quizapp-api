0f640c54ad52dcd7cd05dd81db3c2362a8153f03
Rupert Ellis Brown <ruperteb@gmail.com>
2022-05-25 15:52:04 +0100

    adding cors

--- index.js
+++ index.js
@@ -4,48 +4,9 @@ var cors = require("cors");
 const app = express();
 const keycloak = require("./config/keycloak-config.js").initKeycloak();
 
-/* var allowCrossDomain = function (req, res, next) {
-  res.header("Access-Control-Allow-Origin", "*");
-  res.header("Access-Control-Allow-Methods", "GET,PUT,POST,DELETE");
-  res.header("Access-Control-Allow-Headers", "Content-Type, Authorization");
-
-  // intercept OPTIONS method
-  if ("OPTIONS" == req.method) {
-    res.send(200);
-  } else {
-    next();
-  }
-};
-app.use(allowCrossDomain); */
-
-app.use(cors());
+app.use(cors({ origin: ["http://localhost:3000/"] })); // ! must be before keycloak middleware !
 
 app.use(keycloak.middleware());
-
-/* app.use(
-  cors({
-    credentials: true,
-    origin: ["http://localhost:3000/"],
-  })
-); */
-
-/* app.use(cors()); */
-
-/* app.use(
-  cors({
-    origin: true,
-    optionsSuccessStatus: 200,
-    credentials: true,
-  })
-);
-app.options(
-  "*",
-  cors({
-    origin: true,
-    optionsSuccessStatus: 200,
-    credentials: true,
-  })
-); */
 
 const users = require("./routes/users");
 const quizzes = require("./routes/quizzes");
