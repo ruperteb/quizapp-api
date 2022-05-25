24f0dbcb3d7e7f214fc4b9aef5c858cf2515e28f
Rupert Ellis Brown <ruperteb@gmail.com>
2022-05-25 15:29:37 +0100

    adding cors

--- routes/users.js
+++ routes/users.js
@@ -3,11 +3,14 @@ const express = require("express");
 const { jsonParser } = require("../middleware/bodyparser");
 const router = express.Router();
 
-const keycloak = require('../config/keycloak-config.js').getKeycloak();
+var cors = require("cors");
+
+const keycloak = require("../config/keycloak-config.js").getKeycloak();
 
 var usersController = require("../controllers/users");
 
-router.all("*", keycloak.protect(['quizTaker', 'quizMaker']), jsonParser);
+router.all("*", keycloak.protect(["quizTaker", "quizMaker"]), jsonParser);
+router.options("/getUserWithToken", cors());
 router.get("/getUser", usersController.getUser);
 router.get("/getUserWithToken", usersController.getUserWithToken);
 router.get("/getUsers", usersController.getUsers);
