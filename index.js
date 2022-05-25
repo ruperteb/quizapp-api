beb59d932f5b99c7451a93ee946374edb825efa5
Rupert Ellis Brown <ruperteb@gmail.com>
2022-05-25 14:44:59 +0100

    adding cors

--- index.js
+++ index.js
@@ -15,6 +15,20 @@ app.use(keycloak.middleware());
 
 /* app.use(cors()); */
 
+app.use((req, res, next) => {
+  //doesn't send response just adjusts it
+  res.header("Access-Control-Allow-Origin", "*"); //* to give access to any origin
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept, Authorization" //to give access to all the headers provided
+  );
+  if (req.method === "OPTIONS") {
+    res.header("Access-Control-Allow-Methods", "PUT, POST, PATCH, DELETE, GET"); //to give access to all the methods provided
+    return res.status(200).json({});
+  }
+  next(); //so that other routes can take over
+});
+
 const users = require("./routes/users");
 const quizzes = require("./routes/quizzes");
 const questions = require("./routes/questions");
