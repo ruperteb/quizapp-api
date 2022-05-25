90636814298489f928925503743810148de809b9
Rupert Ellis Brown <ruperteb@gmail.com>
2022-05-25 15:39:33 +0100

    adding cors

--- index.js
+++ index.js
@@ -1,14 +1,24 @@
 require("dotenv").config();
 const express = require("express");
-var cors = require("cors");
+/* var cors = require("cors"); */
 const app = express();
 const keycloak = require("./config/keycloak-config.js").initKeycloak();
 
 app.use(keycloak.middleware());
 
-app.options("/*/*", cors());
+var allowCrossDomain = function (req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Methods", "GET,PUT,POST,DELETE");
+  res.header("Access-Control-Allow-Headers", "Content-Type, Authorization");
 
-app.use(cors());
+  // intercept OPTIONS method
+  if ("OPTIONS" == req.method) {
+    res.send(200);
+  } else {
+    next();
+  }
+};
+app.use(allowCrossDomain);
 
 /* app.use(
   cors({
--- routes/users.js
+++ routes/users.js
@@ -3,14 +3,11 @@ const express = require("express");
 const { jsonParser } = require("../middleware/bodyparser");
 const router = express.Router();
 
-var cors = require("cors");
-
-const keycloak = require("../config/keycloak-config.js").getKeycloak();
+const keycloak = require('../config/keycloak-config.js').getKeycloak();
 
 var usersController = require("../controllers/users");
 
-router.all("*", keycloak.protect(["quizTaker", "quizMaker"]), jsonParser);
-router.options("/getUserWithToken", cors());
+router.all("*", keycloak.protect(['quizTaker', 'quizMaker']), jsonParser);
 router.get("/getUser", usersController.getUser);
 router.get("/getUserWithToken", usersController.getUserWithToken);
 router.get("/getUsers", usersController.getUsers);
