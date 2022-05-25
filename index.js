e7ccee2f892909ee518043086416c5cdfa2de1ec
Rupert Ellis Brown <ruperteb@gmail.com>
2022-05-25 13:03:15 +0100

    adding cors

--- index.js
+++ index.js
@@ -10,11 +10,14 @@ const users = require("./routes/users");
 const quizzes = require("./routes/quizzes");
 const questions = require("./routes/questions");
 
-app.use(
-  cors({
-    origin: "*",
-  })
-);
+var allowCrossDomain = function (req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Methods", "GET,PUT,POST,DELETE");
+  res.header("Access-Control-Allow-Headers", "Content-Type");
+  next();
+};
+
+app.use(allowCrossDomain);
 
 app.use("/users/", users);
 app.use("/quizzes/", quizzes);
