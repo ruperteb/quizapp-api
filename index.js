fd56e3cb90f83dfe3bf6428579121c4e7886dd2b
Rupert Ellis Brown <ruperteb@gmail.com>
2022-05-25 12:31:39 +0100

    adding cors

--- index.js
+++ index.js
@@ -1,14 +1,20 @@
-require('dotenv').config()
+require("dotenv").config();
 const express = require("express");
+var cors = require("cors");
 const app = express();
-const keycloak = require('./config/keycloak-config.js').initKeycloak();
+const keycloak = require("./config/keycloak-config.js").initKeycloak();
+
+app.use(
+  cors({
+    origin: ["http://localhost:3000/"],
+  })
+);
+
 app.use(keycloak.middleware());
 
 const users = require("./routes/users");
 const quizzes = require("./routes/quizzes");
 const questions = require("./routes/questions");
-
-
 
 app.use("/users/", users);
 app.use("/quizzes/", quizzes);
