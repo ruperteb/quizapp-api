9ba2505a90713eceed8a5616b664bfa46d4e29ca
Rupert Ellis Brown <ruperteb@gmail.com>
2022-05-25 13:23:11 +0100

    adding cors

--- index.js
+++ index.js
@@ -1,27 +1,20 @@
 require("dotenv").config();
 const express = require("express");
-/* var cors = require("cors"); */
+var cors = require("cors");
 const app = express();
 const keycloak = require("./config/keycloak-config.js").initKeycloak();
 
 app.use(keycloak.middleware());
 
+app.use(cors());
+
 const users = require("./routes/users");
 const quizzes = require("./routes/quizzes");
 const questions = require("./routes/questions");
 
-var allowCrossDomain = function (req, res, next) {
-  res.header("Access-Control-Allow-Origin", "*");
-  res.header("Access-Control-Allow-Methods", "GET,PUT,POST,DELETE");
-  res.header("Access-Control-Allow-Headers", "Content-Type");
-  next();
-};
-
 app.use("/users/", users);
 app.use("/quizzes/", quizzes);
 app.use("/questions/", questions);
-
-app.use(allowCrossDomain);
 
 app.listen(process.env.PORT, () => {
   console.log(`Example app listening on port ${process.env.PORT}`);
