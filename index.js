b089442a9238fd2ace76041031f4442c97455398
Rupert Ellis Brown <ruperteb@gmail.com>
2022-05-25 13:05:16 +0100

    adding cors

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 require("dotenv").config();
 const express = require("express");
-var cors = require("cors");
+/* var cors = require("cors"); */
 const app = express();
 const keycloak = require("./config/keycloak-config.js").initKeycloak();
 
@@ -10,14 +10,14 @@ const users = require("./routes/users");
 const quizzes = require("./routes/quizzes");
 const questions = require("./routes/questions");
 
-var allowCrossDomain = function (req, res, next) {
+/* var allowCrossDomain = function (req, res, next) {
   res.header("Access-Control-Allow-Origin", "*");
   res.header("Access-Control-Allow-Methods", "GET,PUT,POST,DELETE");
   res.header("Access-Control-Allow-Headers", "Content-Type");
   next();
 };
 
-app.use(allowCrossDomain);
+app.use(allowCrossDomain); */
 
 app.use("/users/", users);
 app.use("/quizzes/", quizzes);
